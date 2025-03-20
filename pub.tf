e7d4135e53649f6b1aa7bd6615ed5cf739a89a51
Deepanshu Kumar <86040514+anshuk6469@users.noreply.github.com>
2025-03-20 16:43:55 +0530

    Update pub.tf

--- pub.tf
+++ pub.tf
@@ -8,3 +8,14 @@ resource "azurerm_public_ip" "example" {
     environment = "Production"
   }
 }
+
+resource "azurerm_public_ip" "example2" {
+  name                = "Deep-ip2"
+  resource_group_name = azurerm_resource_group.rgprod.name
+  location            = azurerm_resource_group.rgprod.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Production"
+  }
+}
