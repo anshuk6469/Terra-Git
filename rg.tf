801030686a14dd2615c994f8011c33ff9a891397
Deepanshu Kumar <anshuk6469@gmail.com>
2025-03-20 16:28:50 +0530

    new resource group added

--- rg.tf
+++ rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rgprod" {
    name =  "deep-rg1"
    location = "westus"
 }
+
+resource "azurerm_resource_group" "rgprod2" {
+   name =  "deep-rg2"
+   location = "westus"
+}
